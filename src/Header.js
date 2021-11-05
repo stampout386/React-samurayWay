c941c67273ddeed165e0bc8a974ec457937fe13a
Mikhail Shestak <stampout386@gmail.com>
2021-11-05 19:48:09 +0300

    public

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+      <div>
+         <a href="#s">Home </a> -
+         <a href="#s">News </a> -
+         <a href="#s">Messages</a> -
+      </div>
+    )
+  }
+
+export default Header;
---
+++ src/Tehnologies.js
@@ -0,0 +1,12 @@
+function Tehnologies(){
+    return(
+      <ul>
+         <li>css</li>
+         <li>html</li>
+         <li>js</li>
+         <li>react</li>
+       </ul>
+    )
+  }
+
+  export default Tehnologies;
